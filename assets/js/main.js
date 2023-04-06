f1e3d891aab3476bc002e2a39cc267a3bc0edb5f
Yonesky <yonessky.yonesskysimamora@gmail.com>
2023-04-06 13:08:04 +0700

    eskopi luar

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    //WHen the scroll is greater than 50 viewport height, add the scroll-header class
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+                       : header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
